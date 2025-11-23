3f2f39337a6d2239bc5e3d74c2e18a565d3bcfad
Julio Cesar <jucecr@hotmail.com>
2025-11-23 17:28:18 -0300

    a

--- aula 4/aula4.js
+++ aula 4/aula4.js
@@ -4,7 +4,7 @@ Somente quando solicitado (Parente gente boa)
 - Trecho de código que pode ser Reutilizado várias vezes
 
 [ ] O que é uma função e como usar
-[ ] Funcão VOID (vazia)
+[ ] Funcão VOID (vazia) / Não RTORNA nada
 [ ] Funcão com parâmetros
 [ ] Função Return
 [ ] Arrow Function
@@ -13,17 +13,52 @@ Somente quando solicitado (Parente gente boa)
 const number = 200
 console.log(number)
 
+
 /*void */
-function digaMeuNome(){
+function digaMeuNome() {
     const name = "Julio Cesar da Cruz"
     console.log(name)
 }
+digaMeuNome()
+
 
 /* parâmetros*/
 
 
-
-function digaMeuNome(){
-    const name = "Julio Cesar da Cruz"
-    console.log(name)
+function soma(numero1, numero2) {
+    const resultado = numero1 + numero2
+    console.log(resultado)
 }
+soma(20, 10)
+/* return */
+
+function soma(numero1, numero2) {
+    const resultado = numero1 + numero2
+    return resultado
+}
+
+const resultadoDaSoma = soma(30, 40)
+console.log(resultadoDaSoma)
+
+function estaEndividado(receita, gastos) {
+    if (receita > gastos) {
+        return "Está no AZUL"
+    } else {
+        return "Está no VERMELHO"
+    }
+}
+
+const maria = estaEndividado(5000, 7000)
+const joao = estaEndividado(10000, 2000)
+
+console.log(maria)
+console.log(joao)
+
+
+/* Arrow Function (função Flecha) */
+
+function abacate(){}  //fora classica
+const digaMeuNome2 = () => { //Forma moderna
+    console.log("Julio")
+}
+digaMeuNome2()
