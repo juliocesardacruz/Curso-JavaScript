4c07d818c1f9759f3a4315596d5e972dea6f7809
Julio Cesar <jucecr@hotmail.com>
2025-11-27 20:48:34 -0300

    Aula de MATH

---
+++ aula 7 - MATH/math.js
@@ -0,0 +1,37 @@
+/*
+ Math -> Matemática
+     - pow(2,2) /potência / 2² = 4
+     - sqrt(25,2) / raiz quadrada / √25
+     - ceil / teto -> arredondar pra cima 
+     - floor / chão -> arredonda pra baixo
+     - Valor de PI
+     - random() / número aleatório entre 0 e 1
+      - Math.random() * (max - min) + min,
+*/
+
+const calculo = Math.pow(2,5)
+console.log(calculo)
+
+const calculo = Math.sqrt(25,2)
+console.log(calculo)
+
+const calculo = Math.sqrt(125,2)
+console.log(Math.ceil(calculo))
+
+const calculo = Math.sqrt(125,2)
+console.log(Math.floor(calculo))
+
+const calculo = Math.PI
+console.log(calculo)
+
+const calculo = Math.random()
+console.log(calculo)
+
+const calculo = Math.random() * (100-1) + 1;
+console.log(calculo)
+
+
+
+
+const calculo = Math.random() * (100-1) + 1;
+console.log(Math.(calculo))
